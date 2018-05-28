abfd4921cc53f415b27c557e57a8c21daaf8678d
sirap1 <39546214+sirap1@users.noreply.github.com>
2018-05-28 18:51:44 +0000

    Update clickads.js

--- clickads.js
+++ clickads.js
@@ -1,7 +1,4 @@
 
-<div class="paris">Movie Jepang</div>
-<div class='listanim-wrapper'>
-
 var numposts = 999;
 var standardstyling = true;
 function showrecentposts(json) { for (var i = 0; i < numposts; i++) { 
@@ -12,11 +9,5 @@ for (var k = 0; k < entry.link.length; k++) {
 if (entry.link[k].rel == 'alternate') { posturl = entry.link[k].href; break; }} posttitle = posttitle.link(posturl); 
 if (standardstyling) document.write('<li>'); document.write(posttitle);} if (standardstyling) document.write('</li>'); } 
 
-
-<div class='content-list'>
-
-<ul>
 <script src="https://noroianime.blogspot.co.id/feeds/posts/default?orderby=published&amp;alt=json-in-script&amp;callback=showrecentposts&amp;max-results=6"></script>
-</ul>
-</div>
-</div>
+
