e1431d69189e47cb8761bb759e5dd588f5bf12af
sirap1 <39546214+sirap1@users.noreply.github.com>
2018-05-28 19:13:44 +0000

    Update noroianime.js

--- noroianime.js
+++ noroianime.js
@@ -1,1 +1,1 @@
-"https://noroianime.blogspot.co.id/feeds/posts/default?orderby=published&amp;alt=json-in-script&amp;callback=showrecentposts&amp;max-results=6"
+//noroianime.blogspot.co.id/feeds/posts/default?orderby=published&amp;alt=json-in-script&amp;callback=showrecentposts&amp;max-results=6
