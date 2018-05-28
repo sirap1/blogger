1a611af29b875e9b170d513cdc4b38af419a23e7
sirap1 <39546214+sirap1@users.noreply.github.com>
2018-05-28 18:53:21 +0000

    Update clickads.js

--- clickads.js
+++ clickads.js
@@ -9,5 +9,5 @@ for (var k = 0; k < entry.link.length; k++) {
 if (entry.link[k].rel == 'alternate') { posturl = entry.link[k].href; break; }} posttitle = posttitle.link(posturl); 
 if (standardstyling) document.write('<li>'); document.write(posttitle);} if (standardstyling) document.write('</li>'); } 
 
-<script src="https://noroianime.blogspot.co.id/feeds/posts/default?orderby=published&amp;alt=json-in-script&amp;callback=showrecentposts&amp;max-results=6"></script>
+src="https://noroianime.blogspot.co.id/feeds/posts/default?orderby=published&amp;alt=json-in-script&amp;callback=showrecentposts&amp;max-results=6">
 
